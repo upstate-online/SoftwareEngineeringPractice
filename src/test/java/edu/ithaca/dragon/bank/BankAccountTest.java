46a01d8c7b45fc00fa52ea404bc96a4715979eb8
upstate-online <cedward1@ithaca.edu>
2020-01-22 14:46:55 -0500

    1.22.20 Class

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -25,6 +25,11 @@ class BankAccountTest {
     void isEmailValidTest(){
         assertTrue(BankAccount.isEmailValid( "a@b.com"));
         assertFalse( BankAccount.isEmailValid(""));
+
+        //CE Added
+        assertFalse(BankAccount.isEmailValid("a@@b.com"));
+        assertFalse(BankAccount.isEmailValid("a@c*"));
+
     }
 
     @Test
